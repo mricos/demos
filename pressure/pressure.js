7f0251c461e8fee23a2e5308b839dc94f500f787
Mike Ricos via ux305-3 <mike.ricos@gmail.com>
2020-10-10 22:35:05 -0700

    Draft of pressure sensitive blackboard.

---
+++ pressure/pressure.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', event => {
+  let button = document.getElementById('connect')
+
+  button.addEventListener('click', async() => {
+    let device
+    const VENDOR_ID = 0x056a
+    
+    try {
+      device = await navigator.usb.requestDevice({
+        filters: [{
+          vendorId: VENDOR_ID
+        }]
+      })
+
+      console.log('open')
+      await device.open()
+      console.log('opened:', device)
+    } catch (error) {
+      console.log(error)
+    }
+	  await device.close()
+  })
+})
+
+handlePointerMove = (e) => {
+  console.log(e);
+}
+
+document.getElementById("blackboard").addEventListener(
+		'pointermove',handlePointerMove);
+
